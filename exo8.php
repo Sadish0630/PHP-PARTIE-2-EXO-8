2626086f2e25b6bddfd5c5449a4f1071efdf911f
audemar <audemarsadish@gmail.com>
2021-12-08 11:13:42 +0100

    exo8 termine

---
+++ exo8.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css" />
+    <title>PARTIE 2 EXO 8</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $isOk = 1;
+            if ($isOk == 0) {
+                echo 'c\'est ok !!';}
+            else {
+                echo 'c\'est pas bon !!!';}
+        ?>
+    </p>
+
+</body>
+
+</html>
